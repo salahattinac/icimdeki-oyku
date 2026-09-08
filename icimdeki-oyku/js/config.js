670f77f5c7bdd552399a77b8069cdcf3dfef7c91
Salahattin Açıkgöz <lsalahattinl@gmail.com>
2026-09-08 15:15:23 +0300

    Update config.js

--- icimdeki-oyku/js/config.js
+++ icimdeki-oyku/js/config.js
@@ -2,9 +2,9 @@
 const SITE_CONFIG = {
   brandName: "İçimdeki Öykü",
   tagline: "Psikolojik Danışmanlık & Kişisel Gelişim",
-  phoneRaw: "05XX XXX XX XX ",
-  phoneDisplay: "05XX XXX XX XX ,
-  phoneTel: "05XX XXX XX XX ",
+    phoneRaw: "05XX XXX XX XX",
+  phoneDisplay: "05XX XXX XX XX",
+  phoneTel: "05XX XXX XX XX",
   instagramHandle: "icimdeki.oyku",
   instagramUrl: "https://www.instagram.com/icimdeki.oyku",
   whatsappDefaultMessage: "Merhaba İçimdeki Öykü, psikolojik danışmanlık süreci hakkında bilgi almak istiyorum."
